48d3a1870ff11a548602e421fbb15c0c104ca9c0
Vaclav <hrabikv@students.zcu.cz>
2022-03-06 13:27:21 +0100

    2.

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import h5py
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("ahoj")
+
+
+# f = h5py.File('../materialy/VarekaGTNEpochs.mat', 'r')
+# data = f.get('data/variable1')
+# data = np.array(data)
+# print(data)
 
 
 # Press the green button in the gutter to run the script.
